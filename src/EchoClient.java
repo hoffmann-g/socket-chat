cef7e75d1b6d5cc1dbfaf6c9d4ae66dad731587b
Guilherme <g.hoffmann.hbs@gmail.com>
2023-07-05 12:52:02 -0300

    comments made

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -7,18 +7,23 @@ import java.net.Socket;
 public class EchoClient {
     public static void main(String[] args){
         try{
-            System.out.println("Client started");
+            System.out.println("[Client]: Started.");
 
+            //socket
             Socket socket = new Socket("localhost", 1720);
+
+            //creates a stream to send the bytes
             OutputStream outputStream = socket.getOutputStream();
+            //writes an object to a bytestream
             ObjectOutputStream objOutputStream = new ObjectOutputStream(outputStream);
 
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-
-            System.out.println("Enter a string: ");
+            System.out.println("[CLIENT]: Enter a string: ");
             String str = userInput.readLine();
 
+            //writes an object to the objOutputStream
             objOutputStream.writeObject(str);
+            //forces it to send the stream
             objOutputStream.flush();
 
         } catch (Exception e){
--- src/EchoServer.java
+++ src/EchoServer.java
@@ -4,18 +4,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-
     public static void main(String[] args){
         try{
-            System.out.println("server starting...");
-
+            System.out.println("[SERVER]: Starting...");
+            //sockets
             ServerSocket server = new ServerSocket(1720);
             Socket clientSocket = server.accept();
+
+            //receives bytes from client
             InputStream inputStream = clientSocket.getInputStream();
+            //converts the bytes to a readable format
             ObjectInputStream objInputStream = new ObjectInputStream(inputStream);
 
-            System.out.println("connection established");
-
+            //reads the inputStream and allocates in a variable
             String receivedStr = (String) objInputStream.readObject();
 
             System.out.println(receivedStr.toUpperCase());
