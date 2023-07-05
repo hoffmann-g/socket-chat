79457e0de9ee1a4f389a4d9668bba96c585cc6eb
Guilherme <g.hoffmann.hbs@gmail.com>
2023-07-05 11:56:00 -0300

    server and client configured

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -1,16 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class EchoClient {
-    
     public static void main(String[] args){
         try{
             System.out.println("Client started");
 
+            Socket socket = new Socket("localhost", 1720);
+            OutputStream outputStream = socket.getOutputStream();
+            ObjectOutputStream objOutputStream = new ObjectOutputStream(outputStream);
+
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.println("Enter a number: ");
+            System.out.println("Enter a string: ");
             String str = userInput.readLine();
+
+            objOutputStream.writeObject(str);
+            objOutputStream.flush();
 
         } catch (Exception e){
             e.printStackTrace();
--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,3 +1,27 @@
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
 
+    public static void main(String[] args){
+        try{
+            System.out.println("server starting...");
+
+            ServerSocket server = new ServerSocket(1720);
+            Socket clientSocket = server.accept();
+            InputStream inputStream = clientSocket.getInputStream();
+            ObjectInputStream objInputStream = new ObjectInputStream(inputStream);
+
+            System.out.println("connection established");
+
+            String receivedStr = (String) objInputStream.readObject();
+
+            System.out.println(receivedStr.toUpperCase());
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
