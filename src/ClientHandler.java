89eef98dd3779919ff79dc5c5d25be81605acde2
Guilherme <g.hoffmann.hbs@gmail.com>
2023-07-07 14:48:10 -0300

    adding data transmission

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,18 +1,41 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ClientHandler{
+public class ClientHandler implements Runnable{
     
     private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
 
-    private static List<ClientHandler> clients = new ArrayList<>(1);
+    //static cause it's a class variable
+    public static List<ClientHandler> clients = new ArrayList<>();
 
     public ClientHandler(Socket socket){
+        try{
+            this.socket = socket;
+            this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            this.out = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
+            clients.add(this);
+            System.out.println(socket + " connected");
+
+        } catch (IOException e){
+
+        }
         
-        this.socket = socket;
-        System.out.println(socket + " connected");
     }
 
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
 
 }
--- src/EchoServer.java
+++ src/EchoServer.java
@@ -15,12 +15,11 @@ public class EchoServer{
         System.out.println("server starting...");
         try {
             Socket socket = serverSocket.accept();
-            System.out.println("connected");
 
             ClientHandler clientHandler = new ClientHandler(socket);
         } catch (IOException e) {
             System.out.println("server is full");
-        } 
+        }
     }
 
     public static void main(String[] args) throws IOException{
